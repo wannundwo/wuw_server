66868313b10d5ef5afd82b3e92facbae44cab407
zirkler <zirkler@users.noreply.github.com>
2015-02-26 20:28:46 +0100

    allow CORS (cross origin resource sharing)

--- api.js
+++ api.js
@@ -23,6 +23,12 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// allow cross origin resource sharing
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // connect to mongodb
 mongoose.connect("mongodb://localhost:27017/wuw");
