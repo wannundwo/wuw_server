96fcdc72f692ec0272ee3e34ef6fc1883b07ab5d
zirkler <zirkler@users.noreply.github.com>
2015-03-24 18:00:10 +0100

    test creation of deadlines

--- test/apiTest.js
+++ test/apiTest.js
@@ -24,24 +24,58 @@ describe('Api', function(){
     assert.equal(typeof api.startApi, 'function');
   });
 
-  it('/ should return 200', function(done) {
-    request("http://localhost:8088/api/v0/", function(error, response, html) {
-      assert.equal(response.statusCode, 200);
-      done();
+  describe('http', function() {
+    it('/ should return 200', function(done) {
+      request("http://localhost:8088/api/v0/", function(error, response, html) {
+        assert.equal(response.statusCode, 200);
+        done();
+      });
     });
-  });
 
-  it('/deadlines should return 200', function(done) {
-    request("http://localhost:8088/api/v0/deadlines/", function(error, response, html) {
-      assert.equal(response.statusCode, 200);
-      done();
+    it('/deadlines should return 200', function(done) {
+      request("http://localhost:8088/api/v0/deadlines/", function(error, response, html) {
+        assert.equal(response.statusCode, 200);
+        done();
+      });
     });
-  });
 
-  it('/lectures should return 200', function(done) {
-    request("http://localhost:8088/api/v0/lectures", function(error, response, html) {
-      assert.equal(response.statusCode, 200);
-      done();
+    it('/lectures should return 200', function(done) {
+      request("http://localhost:8088/api/v0/lectures", function(error, response, html) {
+        assert.equal(response.statusCode, 200);
+        done();
+      });
     });
+
+    var id = "";
+    it('it should create deadline', function(done) {
+        var deadline = {
+            info: 'Do it!',
+            deadline: new Date()
+        };
+
+        var conf = {
+            url: 'http://localhost:8088/api/v0/deadlines/',
+            form: deadline
+        };
+
+        // create a deadline
+        request.post(conf, function(error, response, body) {
+            assert.equal(response.statusCode, 200);
+            id = JSON.parse(body).id;
+            assert.notEqual(typeof id, 'undefined');
+            done();
+        });
+    });
+
+    it('it should have deadline', function(done) {
+        var url = 'http://localhost:8088/api/v0/deadlines/' + id;
+        request(url, function(error, response, html) {
+            assert.equal(response.statusCode, 200);
+            var info = JSON.parse(html).info;
+            assert.equal(info, 'Do it!');
+            done();
+        });
+    });
+
   });
 });
