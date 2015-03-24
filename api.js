d4c4428b5746e6c85d1b4eef72dcb4f8bac4d296
Ben Lebherz <git@benleb.de>
2015-03-24 18:30:14 +0100

    remove useles log message

--- api.js
+++ api.js
@@ -59,7 +59,7 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log("Something is happening...");
+    //console.log("Something is happening...");
     next();
 });
 
