f34fbb7fe77681c09a36ca5df6095a1845a16a73
zirkler <zirkler@users.noreply.github.com>
2015-05-10 09:43:26 +0200

    add weekly route

--- routes/lectures.js
+++ routes/lectures.js
@@ -6,6 +6,8 @@
 // import the express router
 var router = require("express").Router();
 
+var moment = require("moment");
+
 // create model
 var Lecture = require("../models/model_lecture");
 
@@ -54,6 +56,42 @@ router.route("/groups")
         
         var reqGroups = JSON.parse(req.body.groups || "[]");
         var query = { groups: { $in: reqGroups }, "endTime": {"$gte": today}};
+
+        // if no reqGroups provided, return all lectures
+        if (reqGroups.length === 0) {
+            delete query.groups;
+        }
+
+        // check if we got a proper array
+        if (reqGroups) {
+            Lecture.find(query).sort({startTime: 1}).exec(function(err, lectures) {
+                if (err) { res.status(500).send(err + " - data was: " + reqGroups);  }
+                res.status(200).json(lectures);
+                res.end();
+            });
+        } else {
+            res.status(400).json({ error: "ohh that query looks wrong to me: " + reqGroups });
+            return;
+        }
+    });
+    
+// on routes that end in /lectures/weekly
+router.route("/weekly")
+
+    // get lectures for specific groups (POST /$apiBaseUrl/lectures/groups)
+    .post(function(req, res) {
+        
+        var mon = moment().day(1);
+        mon.hour(0);
+        mon.minute(0);
+        mon.seconds(0);
+        var sun = moment().day(7);
+        sun.hour(23);
+        sun.minute(59);
+        sun.seconds(59);
+        
+        var reqGroups = JSON.parse(req.body.groups || "[]");
+        var query = { groups: { $in: reqGroups }, "startTime": {"$gte": mon}, "endTime": {"$lte": sun}};
 
         // if no reqGroups provided, return all lectures
         if (reqGroups.length === 0) {
