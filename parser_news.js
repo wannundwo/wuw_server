9718966549a409c89f8d0d7afe0d93b36aa154df
Ben Lebherz <git@benleb.de>
2015-08-28 20:45:21 +0200

    parser: news parser works with a little hack, fix if you know how

--- parser_news.js
+++ parser_news.js
@@ -6,20 +6,20 @@ var async = require('async');
 var crypto = require('crypto');
 var xml2js = require('xml2js');
 var utils = require('./wuw_utils');
-
+var path = require('path');
 var parseString = xml2js.parseString;
 
 // data source
-//var url = 'http://php.rz.hft-stuttgart.de/hftapp/nachrichtenhftapp.php';
 var url = 'http://localhost:8000/news.xml';
-
 
 // mongodb
 var mongohost='localhost:27017';
 var mongodb=process.env.WUWDB || 'wuw';
 var mongoConnection='mongodb://' + mongohost + '/' + mongodb;
-// connect
-mongoose.connect(mongoConnection);
+
+// running as module or standalone?
+var standalone = !module.parent;
+var scriptName = path.basename(module.filename, path.extname(module.filename));
 
 var startParser = function() {
     // connect to mongodb (if not already)
@@ -30,83 +30,68 @@ var startParser = function() {
     // create model from our schema (needed for drop)
     var News = require('./models/model_news');
 
-    console.log('\n * news parser started\n');
+    console.log('[' + (new Date()) + '] ' + scriptName + ': started with { }');
 
-    // drop current lecture collection to get a fresh result
-    mongoose.connection.collections.news.drop(function(err) {
+    // simple progress display if run as standalone
+    if (standalone) { process.stdout.write(' '); }
+
+    // fetch xml
+    request(url, function(err, response, xml) {
         if(err) { console.log(err); }
+        else {
 
-        console.log('  * dropped old \''+ News.collection.name + '\' collection...');
+            // parse xml
+            parseString(xml, { explicitArray: false, normalize: true, preserveChildrenOrder: true }, function (err, result) {
+                if(err) { console.log(err); }
+                else {
 
-        request(url, function(err, response, xml) {
-            if(err) { console.log(err); }
-            else {
-                console.log('  * got XML data');
+                    // drop current collection to get a fresh result
+                    mongoose.connection.collections.news.drop(function(err) {
+                        if(err) { console.log(err); }
 
-                // parse xml
-                parseString(xml, {preserveChildrenOrder: true}, function (err, result) {
-                    if(err) { console.log(err); }
-                    else {
-                        console.log('  * parsed data to json');
-                        console.log('  * starting import');
-                        console.log();
-
+                        // process each element
                         async.eachLimit(result.nachrichten.nachricht, 5, function(newsEntry, cb) {
 
-                            console.log('------------------------------------------------------');
-                            //console.log(newsEntry);
-                            // create Lecture from our Model
-                            var NewsObj = new News();
+                            // create an object from our model
+                            var newsObj = new News();
                             // set attributes
-                            NewsObj.lectureName = newsEntry.title;
-                            NewsObj.url = newsEntry.url;
-                            NewsObj.descr = newsEntry.description;
-                            NewsObj.text = newsEntry.text;
-                            NewsObj.created = new Date(newsEntry.created);
-                            NewsObj.modified = new Date(newsEntry.modified);
-                            // NewsObj.hashCode = utils.hashCode(Lec.lectureName+Lec.startTime);
-                            // NewsObj._id = mongoose.Types.ObjectId(Lec.hashCode);
+                            newsObj.title = newsEntry.titel;
+                            newsObj.url = newsEntry.url;
+                            newsObj.descr = newsEntry.description;
+                            newsObj.created = new Date(newsEntry.created);
+                            newsObj.modified = new Date(newsEntry.modified);
 
-                            var builder = new xml2js.Builder({headless: true});
-                            var to = builder.buildObject(newsEntry.text[0]);
-                            //console.log(newsEntry.text);
-                            console.log(newsEntry.text[0]);
-                            console.log();
-                            console.log();
-                            console.log(to);
-                            console.log();
-                            console.log();
-                            console.log('------------------------------------------------------');
+                            // get our html back from our json-xml-object - crazy but working
+                            var builder = new xml2js.Builder({ headless: true, renderOpts: { pretty: false } });
+                            // we need a html tag because this is all crazy and wont work without, so we use 'section'
+                            var newsText = { section: newsEntry.text };
+                            var newsTextHTML = builder.buildObject(newsText);
+                            newsObj.text = newsTextHTML;
 
-                            // // create an object from our document
-                            // var upsertData = NewsObj.toObject();
-                            // // delete attributes to upsert
-                            // delete upsertData.rooms;
-                            // delete upsertData.groups;
-                            //
-                            // var room = newsEntry.raum[0];
-                            // var group = newsEntry.semesterverband[0];
-                            //
-                            // console.log(newsEntry.raum + ' -> ' + room);
-                            // console.log(newsEntry.semesterverband + ' -> ' + group);
-                            // console.log();
+                            // this is just inserting, not upserting!
+                            News.update({ title: newsObj.title }, { $set: newsObj.toObject()  }, { upsert: true }, function() {
+                                // simple progress display if run as standalone
+                                if (standalone) { process.stdout.write(' *'); }
+                                cb();
+                            });
 
-                            // // lectures without a group/room are useless...
-                            // if(group !== '' && room !== '') {
-                            //     // save lecture to db & call callback
-                            //     Lecture.update({ _id: Lec.id }, { $set: upsertData, $addToSet: { rooms: room, groups: group }  }, { upsert: true }, cb);
-                            // } else {
-                            //     cb();
-                            // }
+                        }, function() {
+                            // when everything is done, clean up
+                            if (standalone) {
+                                process.stdout.write('\n');
+                                mongoose.disconnect();
+                            }
+                            console.log('[' + (new Date()) + '] ' + scriptName + ': completed successfully');
                         });
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        }
     });
 };
 
-// start the magic
-startParser();
+// immediately start parsing if run as standalone
+if (standalone) { startParser(); }
 
-//module.exports = { startParser: startParser, parse: parse };
+// export function
+module.exports = { startParser: startParser };
