f07b9650e7a8572c16635ad8e0835065f47463cc
TheJetlag <chris@Chriss-MacBook-Pro.local>
2015-10-26 00:28:53 +0100

    groups/lectures: handle if user is null / new

--- routes/groups.js
+++ routes/groups.js
@@ -49,11 +49,10 @@ router.route('/lectures/:user_id')
 
             // get the users selected lectures
             User.findOne({'deviceId': req.params.user_id}, function(err, user) {
-                if (user == null) {
-                    res.status(500).send("user id not found");
+                var usersSelectedLectures = [];
+                if (user) {
+                   var usersSelectedLectures = user.selectedLectures;
                 }
-
-                var usersSelectedLectures = user.selectedLectures;
 
                 for (var i = 0; i < groups.length; i++) {
                     // find out how many lectures the user has selected in this group
